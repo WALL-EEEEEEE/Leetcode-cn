8bbb067f70e078d16e0083edeaf17b52569f4573
root <root@HYGPC.localdomain>
2022-02-21 14:03:57 +0800

    添加左侧单向，和右侧单向的快速选择方法

--- median_in_array.go
+++ median_in_array.go
@@ -1,44 +1,37 @@
 package main
 
-func part_sort(arr []int, left int, right int) int {
-	key := arr[right]
-	for left < right {
-		for left < right && key > arr[left] {
-			left++
-		}
-		for left < right && key < arr[right] {
-			right--
-		}
-		if left < right {
-			tmp := arr[left]
-			arr[left] = arr[right]
-			arr[right] = tmp
+import "fmt"
+
+func pardition(arr []int, left int, right int) int {
+	pivot := arr[right]
+	var i, j int
+	for i, j = left-1, left; j < right; j++ {
+		if arr[j] < pivot {
+			i++
+			arr[i], arr[j] = arr[j], arr[i]
 		}
 	}
-	return left
+	arr[i+1], arr[right] = arr[right], arr[i+1]
+	return i + 1
 }
 
 func median_in_array(arr []int) int {
-	var median_index int
-	if len(arr)%2 == 0 {
-		median_index = len(arr)/2 - 1
-	} else {
-		median_index = len(arr) / 2
-	}
+	var median_index int = len(arr) / 2
 	left, right := 0, len(arr)-1
-	for left < right {
-		if left < median_index {
-			left = part_sort(arr, left+1, right)
+	p := pardition(arr, left, right)
+	for p != median_index {
+		if p < median_index {
+			p = pardition(arr, left+1, right)
 		} else if left > median_index {
-			left = part_sort(arr, 0, left-1)
+			p = pardition(arr, 0, left-1)
 		} else {
 			break
 		}
 	}
 	if len(arr)%2 == 0 {
-		return (arr[left] + arr[left+1]) / 2
+		return (arr[p] + arr[p-1]) / 2
 	}
-	return arr[left]
+	return arr[p]
 }
 
 func main() {
@@ -52,6 +45,6 @@ func main() {
 	}
 	for _, test_case := range test_cases {
 		median := median_in_array(test_case)
-		fmt.printf("数组: %v, 中位数为：%v\n", test_case, median)
+		fmt.Printf("数组: %v, 中位数为：%v\n", test_case, median)
 	}
 }
